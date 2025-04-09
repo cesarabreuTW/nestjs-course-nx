c477e8d0eeb37da254ad6071bd31f83a03d5d168
Cesar Abreu <cesarabreu.thoughtworks@latam.com>
2025-04-09 14:01:20 -0400

    fix(teslo-shop): :bug: Se agregan dependencias para seed de productos
    Este seed sólo esta funcionando para productos

--- apps/teslo-shop/src/app/seed/seed.module.ts
+++ apps/teslo-shop/src/app/seed/seed.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '../feature/products/entities/product.entity';
 import { SeedController } from './seed.controller';
+import { SeedService } from './seed.service';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [TypeOrmModule.forFeature([Product])],
 })
 export class SeedModule {}
--- apps/teslo-shop/src/app/seed/seed.service.ts
+++ apps/teslo-shop/src/app/seed/seed.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from '../feature/products/entities/product.entity';
+import { initialData } from './seed';
 
 @Injectable()
 export class SeedService {
+  private readonly logger = new Logger('SeedService');
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>
   ) {}
-  create() {
-    // this.productRepository.insert(initialData)
-    return 'Seed executed!!';
+  async create() {
+    try {
+      const insertions = initialData.products.map((product) => {
+        const producCreated = this.productRepository.create(product);
+        return this.productRepository.save(producCreated);
+      });
+      await Promise.all(insertions);
+      return 'Seed executed!!';
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('No se pudo!');
+    }
   }
 }
